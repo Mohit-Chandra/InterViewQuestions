4912f300bed4aa1edc658ccd04147d3a00cda65b
Mohit Chandra <mohitchandra1994@gmail.com>
2022-01-09 18:40:22 +0530

    Time: 1 ms (33.83%), Space: 38.6 MB (22.68%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
-      int dir[][] = {{0,1}, {-1, 0}, {0, -1}, {1,0}};
+      int dir[][] = {{0,1},{1,0},{0, -1},{-1, 0}};
       int x=0,y=0,index=0;
       for(char ch : instructions.toCharArray()){
            if(ch == 'G')
@@ -8,9 +8,9 @@ class Solution {
                x+=dir[index][0];
                y+=dir[index][1];
            }else if(ch == 'L'){
-               index = (index+1)%4;
+               index = (index+3)%4;
            }else{
-              index = (index+3)%4;
+              index = (index+1)%4;
            }
        }
         return (x==0 && y== 0) || (index!=0);
