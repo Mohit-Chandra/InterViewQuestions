5130b33c6103ea8dd0ac4a615420a3e9ebf652c7
Mohit Chandra <mohitchandra1994@gmail.com>
2022-01-11 11:21:09 +0530

    Time: 10 ms (12.91%), Space: 42.1 MB (16.14%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,34 +1,14 @@
 class Solution {
-    /**
-    
-    [3,4,2]
-     3 4  
-    [2,2,3,3,3,4]
-    
-    2 2 4 -> 8
-    3 3 3 -> 9
-    4 2 2 -> 8
-    
-    2->4
-    3->9
-    4->4
-    
-    2 -> 8
-    4 -> 4
-    
-    **/
     public int deleteAndEarn(int[] nums) {
-        int sortedNums[] = Arrays.stream(nums).sorted().distinct().toArray();
-        Map<Integer, Integer> costMap = Arrays.stream(nums).boxed()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(i -> i)));
-        System.out.println(costMap);
-        int ppCost = 0,pCost = costMap.get(sortedNums[0]);
-        for(int i=1;i<sortedNums.length;i+=1){
-            int currCost = costMap.get(sortedNums[i]);
-            int c = sortedNums[i]!=sortedNums[i-1]+1 ? pCost+currCost : Math.max(ppCost + currCost, pCost);
-            ppCost = pCost;
-            pCost = c;
+        int[] numsArray = new int[10001];
+        for (int n: nums) numsArray[n] += n;
+        int current = 0;
+        int previous = 0;
+        for (int num: numsArray) {
+            int temp = current;
+            current = Math.max(num + previous, current);
+            previous = temp;
         }
-        return pCost;
+        return current;
     }
 }
