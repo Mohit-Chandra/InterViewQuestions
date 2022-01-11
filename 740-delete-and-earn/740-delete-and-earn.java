0ac98ddbbe655a830e59781b95e63e0b4671d5e4
Mohit Chandra <mohitchandra1994@gmail.com>
2022-01-11 11:46:27 +0530

    Time: 2 ms (81.82%), Space: 38.6 MB (71.44%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,14 +1,16 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int[] numsArray = new int[10001];
-        for (int n: nums) numsArray[n] += n;
-        int current = 0;
-        int previous = 0;
-        for (int num: numsArray) {
-            int temp = current;
-            current = Math.max(num + previous, current);
-            previous = temp;
+        for(int n : nums){
+            numsArray[n]++;
         }
-        return current;
+        int include = 0,exclude = 0;
+        for(int i=0;i<10001;i+=1){
+            int newInclude = exclude+numsArray[i]*i;
+            int newExclude = Math.max(include,exclude);
+            include = newInclude;
+            exclude = newExclude;
+        }
+        return Math.max(include,exclude);
     }
 }
