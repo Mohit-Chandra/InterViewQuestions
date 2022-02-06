1372cb0fac3368a428c944a6a67665ab95c70ae3
Mohit Chandra <mohitchandra1994@gmail.com>
2022-02-06 10:52:42 +0530

    Time: 1 ms (65.03%), Space: 45 MB (6.26%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,24 +1,17 @@
 class Solution {
-    public void adjustElements(int[] nums,int index){
-        for(int i = index+1;i<nums.length;i+=1){
-            nums[i-1] = nums[i];
-        }
-    }
     public int removeDuplicates(int[] nums) {
-        int n = nums.length,i=1,count =1;
-        while(i<n){
-            if(nums[i] == nums[i-1]){
+        int l=1,r = 1,n=nums.length,count =1;
+        while(r<n){
+            if(nums[r] == nums[r-1])
                 count+=1;
-                if(count > 2){
-                    this.adjustElements(nums,i);
-                    i-=1;
-                    n-=1;
-                }
-            }else{
+            else{
                 count = 1;
             }
-            i+=1;
+            if(count <= 2){
+                nums[l++] = nums[r];
+            }
+            r+=1;
         }
-        return n;
+        return l;
     }
 }
