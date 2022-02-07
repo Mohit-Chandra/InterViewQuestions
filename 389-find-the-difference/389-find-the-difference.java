a91311a39d808cfaecdc711a65af2a616e2c817a
Mohit Chandra <mohitchandra1994@gmail.com>
2022-02-07 11:11:35 +0530

    Time: 2 ms (75.61%), Space: 42.1 MB (13.26%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,18 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character,Integer> hMap = new HashMap<>();
-        for(char ch : t.toCharArray()){
-            hMap.merge(ch,1,Integer::sum);
-        }
-        for(char ch : s.toCharArray()){
-            if(hMap.containsKey(ch)){
-                int count = hMap.get(ch)-1;
-                if(count == 0)
-                    hMap.remove(ch);
-                else
-                    hMap.put(ch,count);
-            }
-        }
-        return hMap.keySet().iterator().next();
+        char ch = 0;
+        for(char curr : s.toCharArray())
+            ch^=curr;
+        for(char curr : t.toCharArray())
+            ch^=curr;
+        
+        return ch;
     }
 }
