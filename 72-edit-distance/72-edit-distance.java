7128234618ab12de9117bc6cc76fb652950f8423
Mohit Chandra <mohitchandra1994@gmail.com>
2022-03-12 19:35:09 +0530

    Time: 3 ms (99.13%), Space: 42 MB (72.62%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -5,22 +5,22 @@ class Solution {
         //if one string is empty return n+m
         if(n*m == 0)
             return n+m;
-        int dp[][] = new int[n+1][m+1];
-        for(int i=0;i<n+1;i+=1){
-            dp[i][0] = i;
-        }
-        for(int j=0;j<m+1;j+=1){
-            dp[0][j] = j;
+        int prev[] = new int[m+1];
+        for(int i=0;i<m+1;i+=1){
+            prev[i] = i;
         }
         for(int i=1;i<n+1;i+=1){
+            int curr[] = new int[m+1];
+            curr[0] = i;
             for(int j=1;j<m+1;j+=1){
                 if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }else{
-                    dp[i][j] = 1+Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]));
+                    curr[j] = 1+Math.min(curr[j-1],Math.min(prev[j-1],prev[j]));
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
