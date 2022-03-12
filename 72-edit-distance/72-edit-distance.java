23e987da2646328d0abb97db901dcf8d556c659c
Mohit Chandra <mohitchandra1994@gmail.com>
2022-03-12 19:26:36 +0530

    Time: 7 ms (74.15%), Space: 46 MB (5.48%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -2,19 +2,25 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        Integer dp[][] = new Integer[n][m];
-        return recur(word1,word2,n-1,m-1,dp);
-    }
-    
-    private int recur(String s1,String s2,int n,int m,Integer [][]dp){
-        if(n<0) return m+1;
-        if(m<0) return n+1;
-        if(dp[n][m]!=null)
-            return dp[n][m];
-        if(s1.charAt(n) == s2.charAt(m)){
-            return dp[n][m] = recur(s1,s2,n-1,m-1,dp);
-        }else{
-            return dp[n][m] = 1+Math.min(recur(s1,s2,n,m-1,dp),Math.min(recur(s1,s2,n-1,m,dp),recur(s1,s2,n-1,m-1,dp)));
+        //if one string is empty return n+m
+        if(n*m == 0)
+            return n+m;
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i+=1){
+            dp[i][0] = i;
         }
+        for(int j=0;j<m+1;j+=1){
+            dp[0][j] = j;
+        }
+        for(int i=1;i<n+1;i+=1){
+            for(int j=1;j<m+1;j+=1){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1+Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
