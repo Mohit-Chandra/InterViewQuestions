ff9c9b9be1aff2961b0fb6a4902a5915409586bb
Mohit Chandra <mohitchandra1994@gmail.com>
2022-06-28 21:38:41 +0530

    Time: 1 ms (98.23%), Space: 42.4 MB (44.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -10,11 +10,11 @@ recur(s,3,3)      recur(s,)
     **/
     public int numDecodings(String s) {
         int sLen = s.length();
-        Integer dp[][] = new Integer[sLen+1][sLen+1];
+        Integer dp[] = new Integer[sLen+1];
         return recur(s,0,sLen,dp);
     }
     
-    public int recur(String s,int start,int end,Integer[][] dp){
+    public int recur(String s,int start,int end,Integer[] dp){
         
         if(start<end && s.charAt(start)=='0')
             return 0;
@@ -22,8 +22,8 @@ recur(s,3,3)      recur(s,)
         if(start>=end)
             return 1;
         
-        if(dp[start][end]!=null)
-            return dp[start][end];
+        if(dp[start]!=null)
+            return dp[start];
         
         int ways = recur(s,start+1,end,dp);
         
@@ -32,7 +32,7 @@ recur(s,3,3)      recur(s,)
             ways+=recur(s,start+2,end,dp);
         }
         
-        return dp[start][end] = ways;
+        return dp[start] = ways;
         
     }
 }
