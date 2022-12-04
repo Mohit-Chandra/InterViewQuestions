a9509989b1abdfdd95af2cbc95b72677c2739374
Mohit Chandra <mohitchandra1994@gmail.com>
2022-12-04 20:03:42 +0530

    Time: 12 ms (94.57%), Space: 42.8 MB (98.63%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,27 +1,28 @@
 class Solution {
-    /**
-        word1 = "cabbba", word2 = "abbccc"
-        aabbbc
-        abbccc
-    **/
-    
     public boolean closeStrings(String word1, String word2) {
-        Map<Character,Integer> wordMap1 = new HashMap<>();
-        Map<Character,Integer> wordMap2 = new HashMap<>();
+        if(word1.length()!=word2.length())
+            return false;
+        int wordMap1[] = new int[26];
+        int wordMap2[] = new int[26];
+        int wordBit1 = 0;
+        int wordBit2 = 0;
         for(char ch : word1.toCharArray()){
-            wordMap1.merge(ch,1,Integer::sum);
+            wordMap1[ch-'a']++;
+            wordBit1 = wordBit1 | (1<<(ch-'a'));
         }
         
         for(char ch : word2.toCharArray()){
-            wordMap2.merge(ch,1,Integer::sum);
+            wordMap2[ch-'a']++;
+            wordBit2 = wordBit2 | (1<<(ch-'a'));
         }
-        if(!wordMap1.keySet().equals(wordMap2.keySet()))
+        if(wordBit1!=wordBit2)
             return false;
-        List<Integer> word1FreqList = new ArrayList<>(wordMap1.values());
-        List<Integer> word2FreqList = new ArrayList<>(wordMap2.values());
-        Collections.sort(word1FreqList);
-        Collections.sort(word2FreqList);
-        return word1FreqList.equals(word2FreqList);
-        
+        Arrays.sort(wordMap1);
+        Arrays.sort(wordMap2);
+        for(int i=0;i<26;i+=1){
+            if(wordMap1[i] != wordMap2[i])
+                return false;
+        }
+        return true;
     }
 }
